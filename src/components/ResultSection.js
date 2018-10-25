3283ba4a5f60b72c287d1a389f82c9d3cc98e182
Hye Soo Park <flyjwayur@gmail.com>
2018-10-25 15:26:38 +0300

    Display no result when user input is not in data

--- src/components/ResultSection.js
+++ src/components/ResultSection.js
@@ -1,29 +1,57 @@
 import React from "react";
-import {hexaGenerator} from "../utils/hexaGenerator";
-import {startsWith, includes} from '../utils/searchFunctions';
-import './resultSection.css';
+import { hexaGenerator } from "../utils/hexaGenerator";
+import { startsWith, includes } from "../utils/searchFunctions";
+import "./resultSection.css";
 
-const ResultSection = ({ countries, searchInput,include}) => {
+const ResultSection = ({ countries, searchInput, include }) => {
+  const checkLengthCountries = () => {
+    let lengthCountries = include
+      ? includes(countries, searchInput).length
+      : startsWith(countries, searchInput).length;
+    console.log(lengthCountries);
+    return lengthCountries > 0 ? true : false;
+  };
+  checkLengthCountries();
 
   return (
     <section className="resultSection">
       <div className="countryList">
-        {include ? (
-          <div>
-            {includes(countries, searchInput).map((country,index) => (
-                <div className="eachCountry" style={{backgroundColor: hexaGenerator()}} key={index}>{country}</div>
+        {checkLengthCountries() ? (
+          include ? (
+            <div>
+              {includes(countries, searchInput).map((country, index) => (
+                <div
+                  className="eachCountry"
+                  style={{ backgroundColor: hexaGenerator() }}
+                  key={index}
+                >
+                  {country}
+                </div>
               ))}
-          </div>
+            </div>
+          ) : (
+            <div>
+              {startsWith(countries, searchInput).map((country, index) => (
+                <div
+                  className="eachCountry"
+                  style={{ backgroundColor: hexaGenerator() }}
+                  key={index}
+                >
+                  {country}
+                </div>
+              ))}
+            </div>
+          )
         ) : (
-          <div>
-            {startsWith(countries, searchInput).map((country, index) => (
-                <div className="eachCountry" style={{backgroundColor: hexaGenerator()}} key={index}>{country}</div>
-              ))}
+          <div className="noResult">
+            {" "}
+            <div>^ - ^</div>
+            <p>There is no result</p>{" "}
           </div>
         )}
       </div>
     </section>
   );
-}
+};
 
 export default ResultSection;

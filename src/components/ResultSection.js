fa135a4971b5ca20e8016877bd009763906c78e6
Hye Soo Park <flyjwayur@gmail.com>
2018-11-01 16:44:00 +0200

    Improve way of outputting the conditional content
    in Result section with creating seperate fn before return

--- src/components/ResultSection.js
+++ src/components/ResultSection.js
@@ -4,50 +4,58 @@ import { startsWith, includes } from "../utils/searchFunctions";
 import "./resultSection.css";
 
 const ResultSection = ({ countries, searchInput, include }) => {
+  
   const checkLengthCountries = () => {
     let lengthCountries = include
       ? includes(countries, searchInput).length
       : startsWith(countries, searchInput).length;
     return lengthCountries > 0 ? true : false;
   };
-  checkLengthCountries();
+
+  const displayFilteredCountries = () => {
+    return include ? (
+      <div>
+        {includes(countries, searchInput).map((country, index) => (
+          <div
+            className="eachCountry"
+            style={{ backgroundColor: hexaGenerator() }}
+            key={index}
+          >
+            {country}
+          </div>
+        ))}
+      </div>
+    ) : (
+      <div>
+        {startsWith(countries, searchInput).map((country, index) => (
+          <div
+            className="eachCountry"
+            style={{ backgroundColor: hexaGenerator() }}
+            key={index}
+          >
+            {country}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const displayNoResult = () => {
+    return (
+      <div className="noResult">
+        {" "}
+        <div>^ - ^</div>
+        <p>There is no result</p>{" "}
+      </div>
+    );
+  };
 
   return (
     <section className="resultSection">
       <div className="countryList">
-        {checkLengthCountries() ? (
-          include ? (
-            <div>
-              {includes(countries, searchInput).map((country, index) => (
-                <div
-                  className="eachCountry"
-                  style={{ backgroundColor: hexaGenerator() }}
-                  key={index}
-                >
-                  {country}
-                </div>
-              ))}
-            </div>
-          ) : (
-            <div>
-              {startsWith(countries, searchInput).map((country, index) => (
-                <div
-                  className="eachCountry"
-                  style={{ backgroundColor: hexaGenerator() }}
-                  key={index}
-                >
-                  {country}
-                </div>
-              ))}
-            </div>
-          )
-        ) : (
-          <div className="noResult">
-            {" "}
-            <div>^ - ^</div>
-            <p>There is no result</p>{" "}
-          </div>
-        )}
+        {checkLengthCountries()
+          ? displayFilteredCountries()
+          : displayNoResult()}
       </div>
     </section>
   );
